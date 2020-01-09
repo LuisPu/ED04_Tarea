8f2af7a88be286db0de07e802614c8c50b34bf0d
Luis <Luis@DESKTOP-BTDJM9Q>
2020-01-09 12:54:55 +0100

    Added Javadoc for CCuenta and fixed constructor.

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -1,28 +1,65 @@
 package cuentas;
+
+/**
+ * Esta clase simula una cuenta bancaria.
+ * @author Luis
+ * @version 1.0
+ * @see <a href=https://wiki.eclipse.org/Main_Page> Enlace de test </a>
+ * 
+ */
 public class CCuenta {
 
-
+    /**
+     * Nombre del propietario
+     */
     private String nombre;
+    /**
+     * Número de cuenta separado por guiones
+     */
     private String cuenta;
+    /**
+     * Saldo de la cuenta
+     */
     private double saldo;
+    /**
+     * Tipo de interés
+     */
     private double tipoInteres;
 
+    /**
+     * Constructor por defecto
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor completo
+     * @param nom Nombre del propietario
+     * @param cue Número de cuenta separado por guiones
+     * @param sal Saldo de la cuenta
+     * @param tipo Tipo de interés
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
         setCuenta(cue);
         setSaldo(sal);
+        setTipoInteres(tipo);
     }
 
+    /**
+     * @return Saldo de la cuenta
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * @param cantidad Cantidad a ingresar
+     * @throws Exception Cantidad negativa no válida
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -30,6 +67,10 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * @param cantidad Cantidad a retirar
+     * @throws Exception Cantidad negativa no válida
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -39,34 +80,58 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+    /**
+     * @return Nombre del propietario
+     */
     public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * @param nombre Nombre del propietario
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * @return Número de cuenta separado por guiones
+	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * @param cuenta Número de cuenta separado por guiones
+	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**
+	 * @return Saldo de la cuenta
+	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * @param saldo Saldo de la cuenta
+	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * @return Tipo de interés
+	 */
 	public double getTipoInteres() {
 		return tipoInteres;
 	}
 
+	/**
+	 * @param tipoInteres Tipo de interés
+	 */
 	public void setTipoInteres(double tipoInteres) {
 		this.tipoInteres = tipoInteres;
 	}
